16ee872d4a4c55c657d2c8a713143df28dbd2a64
RaghavRox <raghavrox2002@gmail.com>
2021-11-16 20:47:49 +0530

    some sht

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,12 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from core.main import main
 
 
 class MyApp(App):
     def build(self):
-        return Label(text = "Hello world")
+        return Label(text = main.mainAlgo(13))
 
 
 if __name__ == "__main__" :
---
+++ src/core/main/main.py
@@ -0,0 +1,10 @@
+#from elements.carbon import carbon
+
+#GUI calls this function which returns name and description
+def mainAlgo(lines):
+    return "arey pani chesindi"
+
+
+#takes image and returns image with detected lines and also a list of lines
+def detectLines():
+    pass
