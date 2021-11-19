0e5399b64b0d3faa0fc18210e0a9963571e1db01
RaghavRox <raghavrox2002@gmail.com>
2021-11-20 00:54:34 +0530

    added tests

--- src/core/main/kesh.py
+++ src/core/main/kesh.py
@@ -11,23 +11,36 @@ class kesh:
         maxLength = 0
         
         for leafNode in leafNodes:
-            maxLength = max(kesh.findLongestChain(leafNode,[]),maxLength)
+            maxLength = max(kesh.findLongestChain(leafNode,0),maxLength)
 
         return maxLength
 
     @staticmethod
-    def findLongestChain(node , visitedArray):
-        connectedNodes = node.getConnectedNodes()
-        notVisitedNodes = []
-        for connectedNode in connectedNodes:
-            if connectedNode not in visitedArray:
-                notVisitedNodes.append(connectedNode)
+    def findLongestChain(node ,length, prevNode = None):
+        if len(node.getConnectedNodes())==1 and node.getConnectedNodes()[0]==prevNode:
+                return 1
+        else:
+            for connectedNode in node.getConnectedNodes():
+                if connectedNode is not prevNode:
+                    length = max(kesh.findLongestChain(connectedNode,length,node),length)+1
+            return length
+
+
+
+
+    # @staticmethod
+    # def findLongestChain(node , visitedArray):
+    #     connectedNodes = node.getConnectedNodes()
+    #     notVisitedNodes = []
+    #     for connectedNode in connectedNodes:
+    #         if connectedNode not in visitedArray:
+    #             notVisitedNodes.append(connectedNode)
 
             
-            maxLength=0
+    #         maxLength=0
             
-            for notVisitedNode in notVisitedNodes:
-                visitedArray.append(notVisitedNode)
-                for nvncn in notVisitedNode.getConnectedNodes():
-                    maxLength = max(kesh.findLongestChain(nvncn,visitedArray),maxLength)
-            return maxLength+1
+    #         for notVisitedNode in notVisitedNodes:
+    #             visitedArray.append(notVisitedNode)
+    #             for nvncn in notVisitedNode.getConnectedNodes():
+    #                 maxLength = max(kesh.findLongestChain(nvncn,visitedArray),maxLength)
+    #         return maxLength+1
