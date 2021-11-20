1288f2f77932f87f4901af7c8053dbdbfc80c150
RaghavRox <raghavrox2002@gmail.com>
2021-11-20 22:19:58 +0530

    testing buildozer

--- src/__main__.py
+++ src/__main__.py
@@ -1,24 +1,23 @@
-# import kivy
-# from kivy.app import App
-# from kivy.uix.label import Label
+from kivy.app import App
+from kivy.uix.label import Label
 from core.main import main
 
-# class MyApp(App):
-#     def build(self):
-#         return Label(text = main.mainAlgo([[0,0,1,1],[2,0,1,1]]))
+class OrgChemITSApp(App):
+    def build(self):
+        return Label(text = "lol")
 
 
-# if __name__ == "__main__" :
-#     MyApp().run()
+if __name__ == "__main__" :
+    OrgChemITSApp().run()
 
-testCompounds = [[[0,0,1,1],[1,1,2,2],[1,1,1,0],[1,0,3,2],[1,0,3,1]],[[0,0,1,1]],[[0,0,1,1],[2,0,1,1]],[[0,0,1,1],[2,0,1,1],[2,0,5,5]],[[0,0,1,1],[2,0,1,1],[2,0,5,5],[5,5,9,9]],[[0,0,1,1],[2,0,1,1],[2,0,5,5],[5,5,9,9],[6,7,9,9]],[[0,0,1,1],[2,0,1,1],[2,0,5,5],[5,5,9,9],[6,7,9,9],[5,5,12,12]],[[0,0,1,1],[2,0,1,1],[2,0,5,5],[5,5,9,9],[6,7,9,9],[5,5,12,12],[5,5,78,78]],[[0,0,1,0],[1,0,2,1],[2,1,2,2],[2,2,1,3],[2,2,3,3],[2,1,3,0],[3,0,4,1],[3,0,4,-1]]]
+# testCompounds = [[[0,0,1,1],[1,1,2,2],[1,1,1,0],[1,0,3,2],[1,0,3,1]],[[0,0,1,1]],[[0,0,1,1],[2,0,1,1]],[[0,0,1,1],[2,0,1,1],[2,0,5,5]],[[0,0,1,1],[2,0,1,1],[2,0,5,5],[5,5,9,9]],[[0,0,1,1],[2,0,1,1],[2,0,5,5],[5,5,9,9],[6,7,9,9]],[[0,0,1,1],[2,0,1,1],[2,0,5,5],[5,5,9,9],[6,7,9,9],[5,5,12,12]],[[0,0,1,1],[2,0,1,1],[2,0,5,5],[5,5,9,9],[6,7,9,9],[5,5,12,12],[5,5,78,78]],[[0,0,1,0],[1,0,2,1],[2,1,2,2],[2,2,1,3],[2,2,3,3],[2,1,3,0],[3,0,4,1],[3,0,4,-1]]]
 
-for node in main.mainAlgo(testCompounds[len(testCompounds)-1]):
-    print(node.getCords())
-    x = node.getConnectedNodes()
-    for lol in x:
-        print(lol.getCords())
-    print("next node")
+# for node in main.mainAlgo(testCompounds[len(testCompounds)-1]):
+#     print(node.getCords())
+#     x = node.getConnectedNodes()
+#     for lol in x:
+#         print(lol.getCords())
+#     print("next node")
 
-# for test in testCompounds:
-#     print(main.mainAlgo(test))
+# # for test in testCompounds:
+# #     print(main.mainAlgo(test))
